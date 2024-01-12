118daae84774d51c154ed47771b83aff52b15c8e
isha <ishatanveer1@gmail.com>
2024-01-12 22:35:29 +0500

    create account commit

--- app/src/main/java/com/bignerdranch/android/mad_project/joinInstaFragment.java
+++ app/src/main/java/com/bignerdranch/android/mad_project/joinInstaFragment.java
@@ -2,12 +2,16 @@ package com.bignerdranch.android.mad_project;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 public class joinInstaFragment extends Fragment {
+    private AppCompatButton btn_createAccount;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -15,4 +19,26 @@ public class joinInstaFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_join_insta, container, false);
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+        addCreateAccountFrag();
+    }
+
+    private void addCreateAccountFrag() {
+        btn_createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragments, new createAccountFragment(), "create Account")
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+    public void init(View view){
+        btn_createAccount = view.findViewById(R.id.btn_createAccount);
+    }
+
 }
