47267f3595249a5d23c8af3bbfb3e26ee0574685
Raanakd <rk99005544@gmail.com>
2024-01-15 22:37:41 +0500

    check if user is logged in

--- app/src/main/java/com/bignerdranch/android/mad_project/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/mad_project/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 
 import com.bignerdranch.android.mad_project.joinInstaFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +20,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         fm = getSupportFragmentManager();
         ft = fm .beginTransaction();
-
-        if(savedInstanceState == null){
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            mainFragment fragment = new mainFragment();
+            ft.add(R.id.fragments, fragment, "instagram");
+            //fm.popBackStack("instagram", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            ft.commit();
+        }
+        else{
             joinInstaFragment fragment = new joinInstaFragment();
             ft.add(R.id.fragments, fragment, "instagram");
             fm.popBackStack("instagram", FragmentManager.POP_BACK_STACK_INCLUSIVE);
