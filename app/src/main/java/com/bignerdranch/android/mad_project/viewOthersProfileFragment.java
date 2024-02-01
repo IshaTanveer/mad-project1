ae8b1349bea99ff4a6ad63bfd12a53f0e50a3aca
Raanakd <rk99005544@gmail.com>
2024-02-01 22:32:59 +0500

    implementing view others profile.

--- app/src/main/java/com/bignerdranch/android/mad_project/viewOthersProfileFragment.java
+++ app/src/main/java/com/bignerdranch/android/mad_project/viewOthersProfileFragment.java
@@ -17,7 +17,6 @@ public class viewOthersProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_view_others_profile, container, false);
 
         return view;
