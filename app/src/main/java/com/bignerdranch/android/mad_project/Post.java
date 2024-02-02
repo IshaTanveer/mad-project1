57d300a5824f0db98114c2b9c67d10ac08bceb01
Raanakd <rk99005544@gmail.com>
2024-02-02 11:09:48 +0500

    view post on home design continued.

--- app/src/main/java/com/bignerdranch/android/mad_project/Post.java
+++ app/src/main/java/com/bignerdranch/android/mad_project/Post.java
@@ -2,6 +2,28 @@ package com.bignerdranch.android.mad_project;
 
 public class Post {
     String imageUrl;
+    String postId;
+    String userId;
+    String caption;
+
+    public Post(String imageUrl, String postId, String userId, String caption) {
+        this.imageUrl = imageUrl;
+        this.postId = postId;
+        this.userId = userId;
+        this.caption = caption;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
 
     public Post() {
     }
